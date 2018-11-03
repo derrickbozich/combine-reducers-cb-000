0e8937db75580d9a7f7fe4e9f3bbe524ca09e51e
derrick b <dbozich1@gmail.com>
2018-11-03 17:45:32 +0000

    automatically backed up by learn

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import authorReducer from './authorReducer'
+import bookReducer from './bookReducer'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+    book: bookReducer,
+    author: authorReducer
+  })
+}
+
+export default rootReducer
